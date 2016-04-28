25f9ca317e4f6fe5f28a9d3820d36704f5523316
leiyang-mids <ynglei@gmail.com>
2016-04-27 21:50:58 -0400

    HW13

--- HW13/HalloSparkSubmit.py
+++
@@ -1,8 +0,0 @@
-from pyspark import SparkContext
-
-sc = SparkContext()
-y = sc.textFile('hdfs://localhost:9000/user/leiyang/PageRank-test.txt').cache()
-print y.collect()
-#y.saveAsTextFile('halloSaveFile')
-print sc.appName
-print sc.master
